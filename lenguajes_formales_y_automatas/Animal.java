831508a7c7d018d6e4da38a2a8c990808d7ba189
schronding <schronding@gmail.com>
2025-09-15 12:09:53 -0600

    Commit inicial del repositorio

---
+++ lenguajes_formales_y_automatas/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    public static String IAmDog(){
+        return "I am a dog"; 
+    }
+
+    public static void doStuff(){
+        int a = 5;
+        while (a < 50) {
+            System.out.println("hi!");
+            a ++;
+        }
+    }
+}
---
+++ lenguajes_formales_y_automatas/validador_placas.java
@@ -0,0 +1,3 @@
+public class validador_placas {
+    
+}
